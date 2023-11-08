b257c5f55e4bcb11b7d372b4bf3254eab742ae43
adam <adameve1981zero@gmail.com>
2023-11-08 18:04:51 +0800

    using redis to cache result

--- src/main/java/com/example/spring_boot/controllers/AuthController.java
+++ src/main/java/com/example/spring_boot/controllers/AuthController.java
@@ -3,6 +3,7 @@ package com.example.spring_boot.controllers;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.spring_boot.domain.User;
 import com.example.spring_boot.mapper.UserMapper;
+import com.example.spring_boot.services.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserMapper userMapper;
+    private final RedisService redisService;
 
     @Autowired
-    public AuthController(UserMapper userMapper) {
+    public AuthController(UserMapper userMapper, RedisService redisService) {
         this.userMapper = userMapper;
+        this.redisService = redisService;
     }
 
     @GetMapping("/username/{username}")
     public boolean testUsername(@PathVariable String username) {
-        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("username", username);
-        User user = userMapper.selectOne(queryWrapper);
-        return user != null;
+        Object usernameExist = redisService.get(username);
+        if (usernameExist == null) {
+            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("username", username);
+            User user = userMapper.selectOne(queryWrapper);
+            redisService.save(username, user != null);
+            return user != null;
+        } else {
+            System.out.println("Cache hit.");
+            return (boolean) usernameExist;
+        }
     }
 }
---
+++ src/main/java/com/example/spring_boot/services/RedisService.java
@@ -0,0 +1,23 @@
+package com.example.spring_boot.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    public RedisService(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void save(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+}
